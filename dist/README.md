7c540a1572a85690798fb9187a919b0f54cca943
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-02-20 13:49:01 -0600

    add dist folder and sample readme

---
+++ dist/README.md
@@ -0,0 +1,1 @@
+{"title":"Quizard's Coding Challenge"}
--- index.js
+++ index.js
@@ -4,24 +4,53 @@ const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "📝 What is the title of your project?",
-    "✍ Give a brief description of your project:",
-    "🔨 What are the installation instructions for your project?",
-    "👨‍🏫 Please provide usage information:",
-    "🤳 Would you like to include a screenshot?",
-    "🥂 List any collaborators or acknowledgmenets:",
-    "🎓 Please choose a license",
-    "👋 What are the contribution guidelines for this project?",
-    "🃏 Please include relevant test instructions",
-    "🐙 Enter your GitHub username:",
-    "🐱 Please enter your email address:"
+    "What is the title of your project?",
+    "Give a brief description of your project:",
+    "What are the installation instructions for your project?",
+    "Please provide usage information:",
+    "Would you like to include a screenshot?",
+    "List any collaborators or acknowledgmenets:",
+    "Please choose a license",
+    "What are the contribution guidelines for this project?",
+    "Please include relevant test instructions",
+    "Enter your GitHub username:",
+    "Please enter your email address:"
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./dist/README.md", JSON.stringify(data), (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: "README created!"
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[0]
+        }
+    ])
+}
 
 // Function call to initialize app
-init();
+init()
+    .then((data) => {
+        return writeToFile(data);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
