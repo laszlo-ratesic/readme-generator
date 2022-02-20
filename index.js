d2a0b8b3b616475650f1ad1f840688adbaf5fa66
laszlo-ratesic <88006245+laszlo-ratesic@users.noreply.github.com>
2022-02-20 13:24:36 -0600

    add questions to array

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "📝 What is the title of your project?",
+    "✍ Give a brief description of your project:",
+    "🔨 What are the installation instructions for your project?",
+    "👨‍🏫 Please provide usage information:",
+    "🤳 Would you like to include a screenshot?",
+    "🥂 List any collaborators or acknowledgmenets:",
+    "🎓 Please choose a license",
+    "👋 What are the contribution guidelines for this project?",
+    "🃏 Please include relevant test instructions",
+    "🐙 Enter your GitHub username:",
+    "🐱 Please enter your email address:"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
